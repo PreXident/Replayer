387c46c7f61672b40f77bcc331c6c98e7ba2fa3a
Adam Hujecek <adam.hujecek@icc-atcsolutions.com>
2013-08-29 17:13:42 +0200

    First try with double imaging

--- src/main/java/com/paradoxplaza/eu4/replayer/ReplayerController.java
+++ src/main/java/com/paradoxplaza/eu4/replayer/ReplayerController.java
@@ -7,6 +7,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
 
 /**
  * Controller for the Replayer application.
@@ -19,6 +23,11 @@ public class ReplayerController implements Initializable {
     /** Replayer settings. */
     Properties settings;
 
+    Image provinces;
+    PixelReader reader;
+    WritableImage output;
+    PixelWriter writer;
+
     @Override
     public void initialize(final URL url, final ResourceBundle rb) {
         //
@@ -30,6 +39,23 @@ public class ReplayerController implements Initializable {
      */
     public void setSettings(final Properties settings) {
         this.settings = settings;
-        image.setImage(new Image("file:///" + settings.getProperty("eu4.dir") + "map/provinces.bmp"));
+        provinces = new Image("file:///" + settings.getProperty("eu4.dir") + "map/provinces.bmp");
+        reader = provinces.getPixelReader();
+
+        final int width = (int) provinces.getWidth();
+        final int height = (int) provinces.getHeight();
+
+        //Copy from source to destination pixel by pixel
+        output = new WritableImage(width, height);
+        writer = output.getPixelWriter();
+
+        for (int y = 0; y < height; y++){
+            for (int x = 0; x < width; x++){
+                Color color = reader.getColor(x, y);
+                writer.setColor(x, y, color);
+            }
+        }
+
+        image.setImage(output);
     }
 }
